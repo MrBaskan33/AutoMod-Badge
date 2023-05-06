ddba1f7da2dae859fc80350998a7dbd65712bb00
MrBaskan <mrbaskan33@gmail.com>
2023-05-06 14:38:46 +0300

    Update AutoMod.js

--- AutoMod.js
+++ AutoMod.js
@@ -32,150 +32,144 @@ module.exports = {
 async execute(client, interaction) { 
   
   await interaction.deferReply()
-  
-  if(!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) return await interaction.editeply({content: `AutoMod açmak için **Yönetici** yetkisine sahip olmalısın.`})
+ 
+  if(!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) return await interaction.editReply({content: `AutoMod açmak için **Yönetici** yetkisine sahip olmalısın.`})
 
-  const { guild, options } = interaction
-  const Seçenek = options.getSubcommand()
+  if(interaction.options.getSubcommand() === 'spam-messages') {
+    
+    await interaction.editReply({content: `AutoMod kuralı kuruluyor.`})
+      
+    const Kural = await interaction.guild.autoModerationRules.create({
+      name: 'Spam messages',
+      creatorId: botid,
+      enabled: true,
+      eventType: 1,
+      triggerType: 3,
+      triggerMetadata: {
+        mentionTotalLimit: 5
+      },
+      actions: [
+        {
+          type: 1,
+          metadata: {
+            channel: interaction.channel,
+            durationSeconds: 10,
+            customMessage: 'Your message was blocked by AutoMod.'
+          }
+        }
+      ]
+    }).catch(async hata => {
+      setTimeout(async () => {
+        await interaction.editReply({content: `AutoMod kuralı uygulanırken bir hata meydana geldi. Hata: \`${hata}\``})
+      }, 2000)
+    })
+    setTimeout(async () => {
+      if(!Kural) return 
+      await interaction.editReply({content: `AutoMod kuralı başarılı şekilde oluşturuldu.`})
+    }, 3000)
+  }
+   
+  if(interaction.options.getSubcommand() === 'flagged-words') {
+    
+    await interaction.editReply({content: `AutoMod kuralı kuruluyor.`})
+      
+    const Kural2 = await interaction.guild.autoModerationRules.create({
+      name: 'Flagged words',
+      creatorId: botid,
+      enabled: true,
+      eventType: 1,
+      triggerType: 4,
+      triggerMetadata: {
+      presets: [1, 2, 3]
+    },
+    actions: [
+      {
+        type: 1,
+        metadata: {
+          channel: interaction.channel,
+          durationSeconds: 10,
+          customMessage: 'Your message was blocked by AutoMod.'
+        }
+      }
+    ]
+  }).catch(async hata => {
+    setTimeout(async () => {
+      await interaction.editReply({content: `AutoMod kuralı uygulanırken bir hata meydana geldi. Hata: \`${hata}\``})
+    }, 2000)
+  })
+  setTimeout(async () => {
+    if(!Kural2) return 
+    await interaction.editReply({content: `AutoMod kuralı başarılı şekilde oluşturuldu.`})
+    }, 3000)
+  }
+    
+  if(interaction.options.getSubcommand() === 'mention-spam') {
+    
+    await interaction.editReply({content: `AutoMod kuralı kuruluyor.`})
+      
+    const Kural3 = await interaction.guild.autoModerationRules.create({
+      name: 'Mention spam',
+      creatorId: botid,
+      enabled: true,
+      eventType: 1,
+      triggerType: 5,
+      triggerMetadata: {
+        mentionTotalLimit: 5
+      },
+      actions: [
+        {
+          type: 1,
+          metadata: {
+            channel: interaction.channel,
+            durationSeconds: 10,
+            customMessage: 'Your message was blocked by AutoMod.'
+          }
+        }
+      ]
+    }).catch(async hata => {
+      setTimeout(async () => {
+        await interaction.editReply({content: `AutoMod kuralı uygulanırken bir hata meydana geldi. Hata: \`${hata}\``})
+      }, 2000)
+    })
+    setTimeout(async () => {
+      if(!Kural3) return 
+      await interaction.editReply({content: `AutoMod kuralı başarılı şekilde oluşturuldu.`})
+    }, 3000)
+  }
+    
+  if(interaction.options.getSubcommand() === 'keyword') {
+    
+    await interaction.editReply({content: `AutoMod kuralı kuruluyor.`})
+    const Kelime = interaction.options.getString('kelime')
   
-  switch(Seçenek) {
-    case "spam-messages": {
-      
-      await interaction.editReply({content: `AutoMod kuralı kuruluyor.`})
-      
-      const Kural = await guild.autoModerationRules.create({
-        name: 'Spam messages',
-        creatorId: botid,
-        enabled: true,
-        eventType: 1,
-        triggerType: 3,
-        triggerMetadata: {
-          mentionTotalLimit: 5
-        },
-        actions: [
-          {
-            type: 1,
-            metadata: {
-              channel: interaction.channel,
-              durationSeconds: 10,
-              customMessage: 'Your message was blocked by AutoMod.'
-            }
+    const Kural4 = await interaction.guild.autoModerationRules.create({
+      name: 'Keyword',
+      creatorId: botid,
+      enabled: true,
+      eventType: 1,
+      triggerType: 1,
+      triggerMetadata: {
+        keywordFilter: [`${Kelime}`]
+      },
+      actions: [
+        {
+          type: 1,
+          metadata: {
+            channel: interaction.channel,
+            durationSeconds: 10,
+            customMessage: 'Your message was blocked by AutoMod.'
           }
-        ]
-      }).catch(async hata => {
-        setTimeout(async () => {
-          await interaction.editReply({content: `AutoMod kuralı uygulanırken bir hata meydana geldi. Hata: \`${hata}\``})
-        }, 2000)
-      })
+        }
+      ]
+    }).catch(async hata => {
       setTimeout(async () => {
-        if(!Kural) return 
-        await interaction.editReply({content: `AutoMod kuralı başarılı şekilde oluşturuldu.`})
-      }, 3000)
-    }
-    break
-    case "flagged-words": {
-      
-      await interaction.editReply({content: `AutoMod kuralı kuruluyor.`})
-      
-      const Kural2 = await guild.autoModerationRules.create({
-        name: 'Flagged words',
-        creatorId: botid,
-        enabled: true,
-        eventType: 1,
-        triggerType: 4,
-        triggerMetadata: {
-          presets: [1, 2, 3]
-        },
-        actions: [
-          {
-            type: 1,
-            metadata: {
-              channel: interaction.channel,
-              durationSeconds: 10,
-              customMessage: 'Your message was blocked by AutoMod.'
-            }
-          }
-        ]
-      }).catch(async hata => {
-        setTimeout(async () => {
-          await interaction.editReply({content: `AutoMod kuralı uygulanırken bir hata meydana geldi. Hata: \`${hata}\``})
-        }, 2000)
-      })
-      setTimeout(async () => {
-        if(!Kural2) return 
-        await interaction.editReply({content: `AutoMod kuralı başarılı şekilde oluşturuldu.`})
-      }, 3000)
-    }
-    break
-    case "mention-spam": {
-      
-      await interaction.editReply({content: `AutoMod kuralı kuruluyor.`})
-      
-      const Kural3 = await guild.autoModerationRules.create({
-        name: 'Mention spam',
-        creatorId: botid,
-        enabled: true,
-        eventType: 1,
-        triggerType: 5,
-        triggerMetadata: {
-          mentionTotalLimit: 5
-        },
-        actions: [
-          {
-            type: 1,
-            metadata: {
-              channel: interaction.channel,
-              durationSeconds: 10,
-              customMessage: 'Your message was blocked by AutoMod.'
-            }
-          }
-        ]
-      }).catch(async hata => {
-        setTimeout(async () => {
-          await interaction.editReply({content: `AutoMod kuralı uygulanırken bir hata meydana geldi. Hata: \`${hata}\``})
-        }, 2000)
-      })
-      setTimeout(async () => {
-        if(!Kural3) return 
-        await interaction.editReply({content: `AutoMod kuralı başarılı şekilde oluşturuldu.`})
-      }, 3000)
-    }
-    break
-    case "keyword": {
-      
-      await interaction.editReply({content: `AutoMod kuralı kuruluyor.`})
-      const Kelime = interaction.options.getString('kelime')
-  
-      const Kural4 = await guild.autoModerationRules.create({
-        name: 'Keyword',
-        creatorId: botid,
-        enabled: true,
-        eventType: 1,
-        triggerType: 1,
-        triggerMetadata: {
-          keywordFilter: [`${Kelime}`]
-        },
-        actions: [
-          {
-            type: 1,
-            metadata: {
-              channel: interaction.channel,
-              durationSeconds: 10,
-              customMessage: 'Your message was blocked by AutoMod.'
-            }
-          }
-        ]
-      }).catch(async hata => {
-        setTimeout(async () => {
-          await interaction.editReply({content: `AutoMod kuralı uygulanırken bir hata meydana geldi. Hata: \`${hata}\``})
-        }, 2000)
-      })
-      setTimeout(async () => {
-        if(!Kural4) return 
-        await interaction.editReply({content: `AutoMod kuralı başarılı şekilde oluşturuldu.`})
-      }, 3000)
-    }
-    break
+        await interaction.editReply({content: `AutoMod kuralı uygulanırken bir hata meydana geldi. Hata: \`${hata}\``})
+      }, 2000)
+    })
+    setTimeout(async () => {
+      if(!Kural4) return 
+      await interaction.editReply({content: `AutoMod kuralı başarılı şekilde oluşturuldu.`})
+    }, 3000)
   }
   
   }
